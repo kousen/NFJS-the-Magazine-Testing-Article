8645b8c93f9191297a34f265d076687cb2f74c4d
Ken Kousen <ken.kousen@kousenit.com>
2011-11-02 04:07:27 -0400

    Initial commit

---
+++ src/main/groovy/mjg/chuck_norris.groovy
@@ -0,0 +1,9 @@
+package mjg
+
+import groovy.json.JsonSlurper
+
+def url = 'http://api.icndb.com/jokes/random'
+def json = new JsonSlurper().parseText(url.toURL().text)
+def joke = json?.value?.joke
+assert joke
+println joke
